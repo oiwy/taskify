9a97b65183434d3afc5da5c9f9f292dc774a4f88
Aleksey Popov <mewehi.oiwy@gmail.com>
2023-12-14 13:32:52 +0700

    feat: update main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,13 +3,20 @@ import { ROUTE_PATH } from "@/shared";
 import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./index.css";
 
+const pinia = createPinia();
 const router = createRouter({
   history: createWebHistory(),
   routes: ROUTE_PATH as RouteRecordRaw[],
 });
 const app = createApp(App);
 
-app.use(router).use(ElementPlus).mount("#app");
+app.use(router).use(ElementPlus).use(pinia).mount("#app");
+
+// Добавить Tag в Приоритет из Element Plus
+// Доделать модальное окно по изменению задачи ЧЕРЕЗ КОМПОНЕНТ DIALOG ЕБАНОГО ELEMENT PLUS
+// Cделать модальное окно по добавлению задачи
+// Сделать Notification в модальном окне, ошибках
