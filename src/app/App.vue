4e18193374de5bc1d5a3a1965a19b98469784174
Aleksey Popov <mewehi.oiwy@gmail.com>
2023-12-14 13:32:44 +0700

    feat: update App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,10 +3,9 @@
   <el-main>
     <RouterView></RouterView>
   </el-main>
-  <modalTask></modalTask>
 </template>
 
 <script setup lang="ts">
-import { Header, modalTask } from "@/widgets";
+import { Header } from "@/widgets";
 import { RouterView } from "vue-router";
 </script>
